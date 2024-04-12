ec66f5e11daa33f189c9fdc64bae3fa83c818d51
a_suzuki <a_suzuki@LAPTOP-RUFJDCJ5>
2024-04-12 11:43:40 +0900

    ログイン

--- src/main/java/bean/Account.java
+++ src/main/java/bean/Account.java
@@ -1,5 +1,34 @@
 package bean;
 
-public class Account {
-
+public class Account implements java.io.Serializable {
+	
+	private int id;
+	private String loginname;
+	private String password;
+	private String school_cd;
+	
+	public int getId() {
+		return id;
+	}
+	public String getLoginname() {
+		return loginname;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public String getSchool_cd() {
+		return school_cd;
+	}
+	public void setId(int id) {
+		this.id=id;
+	}
+	public void setLoginname(String loginname) {
+		this.loginname=loginname;
+	}
+	public void setPassword(String password) {
+		this.password=password;
+	}
+	public void setSchool_cd(String school_cd) {
+		this.school_cd=school_cd;
+	}
 }
--- src/main/java/dao/AccountDAO.java
+++ src/main/java/dao/AccountDAO.java
@@ -1,5 +1,36 @@
 package dao;
 
-public class AccountDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+import bean.Account;
+
+public class AccountDAO extends DAO {
+
+	public Account serch(String loginname,String password)
+	throws Exception {
+		Account account=null;
+
+		Connection con=getConnection();
+		
+		PreparedStatement st;
+		st=con.prepareStatement(
+				"select * from account where loginname=? and password=?");
+		st.setString(1, loginname);
+		st.setString(2, password);
+		ResultSet rs=st.executeQuery();
+		
+		while (rs.next()) {
+			account=new Account();
+			account.setId(rs.getInt("id"));
+			account.setLoginname(rs.getString("loginname"));
+			account.setPassword(rs.getString("password"));
+			account.setSchool_cd(rs.getString("school_cd"));
+		}
+		
+		st.close();
+		con.close();
+		return account;
+	}
 }

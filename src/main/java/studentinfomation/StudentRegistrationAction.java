4db24e8c04215326df27b8005e045e18aaebd454
mahiro3016 <masaori09040911@gmail.com>
2024-04-12 15:09:50 +0900

    変更

--- src/main/java/studentinfomation/StudentRegistrationAction.java
+++ src/main/java/studentinfomation/StudentRegistrationAction.java
@@ -12,30 +12,35 @@ public class StudentRegistrationAction extends Action{
 		HttpServletRequest request, HttpServletResponse response	
 	) throws Exception {
 		
+		
 		HttpSession session=request.getSession();
 		
-		
-		String name=request.getParameter("name");
-		String no=request.getParameter("no");
-		int ent_year=Integer.parseInt(request.getParameter("ent_year"));		
-		String class_num=request.getParameter("class_num");
-		boolean is_attend = true;
-		String school_cd=request.getParameter("school_cd");
-		
-		Student student=new Student();
-		student.setName(name);
-		student.setNo(no);
-		student.setEnt_year(ent_year);
-		student.setClass_num(class_num);
-		student.setIs_attend(is_attend);
-		student.setSchool_cd(school_cd);
-		
-		
-		StudentDAO dao=new StudentDAO();
-		int line=dao.insert(student);
-		
-		if (line>0) {
-			return "studentregistrationsuccess.jsp";
+		try {
+			String name=request.getParameter("name");
+			String no=request.getParameter("no");
+			int ent_year=Integer.parseInt(request.getParameter("ent_year"));		
+			String class_num=request.getParameter("class_num");
+			boolean is_attend = true;
+			String school_cd=request.getParameter("school_cd");
+			
+			Student student=new Student();
+			student.setName(name);
+			student.setNo(no);
+			student.setEnt_year(ent_year);
+			student.setClass_num(class_num);
+			student.setIs_attend(is_attend);
+			student.setSchool_cd(school_cd);
+			
+			
+			StudentDAO dao=new StudentDAO();
+			int line=dao.insert(student);
+			
+			if (line>0) {
+				return "studentregistrationsuccess.jsp";
+			}
+			
+		} catch (Exception e) {
+			return "registrationerror.jsp";
 		}
 		
 		return "registrationerror.jsp";
---
+++ src/main/webapp/studentinfomation/registrationerror.jsp
@@ -0,0 +1,20 @@
+<%@page contentType="text/html; charset=UTF-8" %>
+<%@include file="../header.html"  %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn" %>
+<link rel="stylesheet" type="text/css" href="../css/style.css">
+<%@include file="../home/menu.jsp" %>
+
+<div id="contents">
+	<h2 class="successtag">登録時にエラーが発生しました。</h2>
+	<p><a href="studentregistration.jsp">戻る</a></p>
+	<p><a hraf="../studentinfomation/StudentInfomation.action">学生一覧</a></p>
+</div>
+
+
+
+
+
+
+
+<%@include file="../footer.html"  %>

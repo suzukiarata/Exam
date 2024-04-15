d7093095087f9fea44a9c2389b99e4949eafd9eb
a_suzuki <a_suzuki@LAPTOP-RUFJDCJ5>
2024-04-15 12:23:53 +0900

    ログアウト

---
+++ src/main/java/studentaccount/LogoutAction.java
@@ -0,0 +1,22 @@
+package studentaccount;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import tool.Action;
+
+public class LogoutAction extends Action {
+	public String execute(
+		HttpServletRequest request, HttpServletResponse response
+	) throws Exception {
+
+		HttpSession session=request.getSession();
+
+		if (session.getAttribute("account")!=null) {
+			session.removeAttribute("account");
+			return "logout-out.jsp";
+		}
+		
+		return "logout-error.jsp";
+	}
+}

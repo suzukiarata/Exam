94137af0d950667d5b0aaacdbdf2eb2981a80003
mahiro3016 <masaori09040911@gmail.com>
2024-04-25 13:42:00 +0900

    a

--- src/main/webapp/js/jsfile.js
+++ src/main/webapp/js/jsfile.js
@@ -1,5 +1,5 @@
 $(window).on('load',function(){
-$("#splash-logo").delay(1500).fadeOut('slow');//ロゴを1.2秒でフェードアウトする記述
+$("#splash-logo").delay(1500).fadeOut('slow');//ロゴをフェードアウトする記述
 	const message1 = document.getElementById('Loading_mes');
 	
 	const messages = [
@@ -23,13 +23,13 @@ $("#splash-logo").delay(1500).fadeOut('slow');//ロゴを1.2秒でフェード�
 	
 	setInterval(showMessage_1, 300);
 
-//=====ここからローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
-$("#splash").delay(1700).fadeOut('slow',function(){//ローディングエリア（splashエリア）を1.5秒でフェードアウトする記述
+//=====ここからローディングエリア（splashエリア）をフェードアウトした後に動かしたいJSをまとめる
+$("#splash").delay(1700).fadeOut('slow',function(){//ローディングエリア（splashエリア）をフェードアウトする記述
 
 $('body').addClass('appear');//フェードアウト後bodyにappearクラス付与
 
 });
-//=====ここまでローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
+//=====ここまでローディングエリア（splashエリア）をフェードアウトした後に動かしたいJSをまとめる
 
 //=====ここから背景が伸びた後に動かしたいJSをまとめたい場合は
 $('.splashbg').on('animationend', function() { 
